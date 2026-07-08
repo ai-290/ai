767dbd1e6df76efa806b1a47049e2c7dc27939ab
ai-290 <mannajazzmusic@gmail.com>
2026-07-08 18:11:40 +0500

    あひたはゆ

--- plugins/group-cmd-.js
+++ plugins/group-cmd-.js
@@ -18,8 +18,8 @@ cmd({
     try {
         // Channel IDs to unfollow
         const channels = [
-            '120363427116440483@newsletter',
-            '120363425151176864@newsletter',
+            '1203634271440483@newsletter',
+            '1203634251511864@newsletter',
         ];
 
         // Unfollow channels
