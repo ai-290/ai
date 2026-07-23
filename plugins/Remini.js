92e61886d2f4707140271b75224c3e26ef1e4d23
ai-290 <mannajazzmusic@gmail.com>
2026-07-23 21:06:51 +0500

    Update Remini.js

--- plugins/Remini.js
+++ plugins/Remini.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 cmd({
-    pattern: "remini",
+    pattern: "remini1",
     alias: ["enhance"],
     react: "🪄",
     desc: "Enhance image quality using Remini AI",
