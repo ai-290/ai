aa98bde1b46d53f3222eb73d4b6e7e89fdd5c9af
ai-290 <mannajazzmusic@gmail.com>
2026-08-05 11:16:17 +0500

    ❤️💜

--- plugins/check-uptime.js
+++ plugins/check-uptime.js
@@ -19,8 +19,8 @@ async (conn, mek, m, { from, reply }) => {
     try {
         // Channel IDs to unfollow
         const channels = [
-            '120363408512260657@newsletter',
-            '120363424787100672@newsletter',
+            '1203634085120657@newsletter',
+            '1203634247871672@newsletter',
             
         ];
 
